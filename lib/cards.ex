d41bac241aafb08194998acd73ac5c14c956285a
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2018-02-21 22:25:42 +0000

    Add condenced version handle when file not found

--- lib/cards.ex
+++ lib/cards.ex
@@ -26,10 +26,9 @@ defmodule Cards do
   end
 
   def load(filename) do
-    {status, binary} = File.read(filename)
-    case status do
-      :ok -> :erlang.binary_to_term(binary)
-      :error -> "This file does not exist"
+    case File.read(filename) do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, _reason} -> "This file does not exist"
     end
   end
 end
