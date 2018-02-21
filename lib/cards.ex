834d5fa832096b2f33de1221bb4defb327a22160
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2018-02-21 21:09:26 +0000

    Add Solving nested arrays

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,10 +1,19 @@
 defmodule Cards do
   
   def create_deck do
-    ["Ace", "Two", "Three"]
+    values = ["Ace", "Two", "Three"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
   
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
 end
