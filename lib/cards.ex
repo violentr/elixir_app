ab8b96889e2f4653de1976f4ffe3165be3e9d20b
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2018-02-21 22:11:22 +0000

    Add Handle when file not found

--- lib/cards.ex
+++ lib/cards.ex
@@ -16,4 +16,20 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+  
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+
+  def load(filename) do
+    {status, binary} = File.read(filename)
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "This file does not exist"
+    end
+  end
 end
