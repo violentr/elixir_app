f0b267e1a296e177e8d092d9948f8ae3ae7b1ea3
Deniss Bohanovs <den.b@itservicesgroup.co.uk>
2018-02-21 22:43:08 +0000

    Add create_hand, what includes 2 methods
    pipe operator used '|>' to pass value to methods
    as first param

--- lib/cards.ex
+++ lib/cards.ex
@@ -31,4 +31,10 @@ defmodule Cards do
       {:error, _reason} -> "This file does not exist"
     end
   end
+
+  def create_hand(hand_size) do
+    Cards.create_deck
+    |> Cards.shuffle
+    |> Cards.deal(hand_size)
+  end
 end
